5d37623482d1eb5a4a456aa3308a7d774d5a05a9
Yin Liu <yin2.liu@sonymobile.com>
2012-12-14 11:48:52 +0800

    Fix memory leak issue in application settings
    If opening Main menu->Settings->Apps, and rotating the phone
    about 50 times, Settings force closes and out of memory occur.
    This is because after ManageApplications onDestroy, references
    of ApplicationsState have not been release. So, they will not
    be garbage collected. It leads to OOM and force close. To
    avoid this we need to release the references of
    ApplicationsState when ManageApplications onDestroy.
    
    Also, when exiting ManageApplications or InstalledApps in
    Settings there is a memory leak due to not releasing some
    objects. This fix releases the Session objects to avoid
    memory leak.
    
    Change-Id: Icca7100bded0653a4ec7f2b4bd48a29262432a3b

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -437,6 +437,12 @@ public class ManageApplications extends Fragment implements
             mRunningProcessesView.setVisibility(View.VISIBLE);
             mLoadingContainer.setVisibility(View.GONE);
         }
+
+        private void releaseTab() {
+            if (mApplications != null) {
+                mApplications.releaseApplicationsAdapter();
+            }
+        }
     }
     private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
     private int mNumTabs;
@@ -830,6 +836,20 @@ public class ManageApplications extends Fragment implements
         public void onMovedToScrapHeap(View view) {
             mActive.remove(view);
         }
+
+        private void releaseApplicationsAdapter() {
+            if (mBaseEntries != null) {
+                mBaseEntries.clear();
+            }
+
+            if (mEntries != null) {
+                mEntries.clear();
+            }
+
+            if (mSession != null) {
+                mSession.release();
+            }
+        }
     }
     
     @Override
@@ -1076,6 +1096,15 @@ public class ManageApplications extends Fragment implements
     @Override
     public void onDestroy() {
         getActivity().unbindService(mContainerConnection);
+
+        if (mApplicationsState != null) {
+            mApplicationsState.releaseApplicationState();
+        }
+
+        for (int i = 0; i < mTabs.size(); i++) {
+            mTabs.get(i).releaseTab();
+        }
+
         super.onDestroy();
     }
 
