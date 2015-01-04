a08488209fdd58f022e10cbac7196b34c261548f
AndroidRul3z <ferrettidario@gmail.com>
2015-01-04 18:28:12 +0100

    Do not allow to access quick settings on secure lockscreen
    Credits to SlimRom
    https://github.com/SlimRoms/packages_apps_Settings/commit/a23400c0cfb37171cf547715a3bf04c24f812a28

--- src/com/android/settings/purity/QuickSettings.java
+++ src/com/android/settings/purity/QuickSettings.java
@@ -17,17 +17,25 @@
 package com.android.settings.purity;
 
 import android.os.Bundle;
-import android.provider.Settings;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
-import android.preference.PreferenceCategory;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.internal.widget.LockPatternUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class QuickSettings extends SettingsPreferenceFragment {
+public class QuickSettings extends SettingsPreferenceFragment
+            implements OnPreferenceChangeListener  {
 
     private static final String TAG = "QuickSettingsPanel";
+
+    private static final String PREF_BLOCK_ON_SECURE_KEYGUARD = "block_on_secure_keyguard";
+
+    SwitchPreference mBlockOnSecureKeyguard;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,10 +44,31 @@ public class QuickSettings extends SettingsPreferenceFragment {
 
         PreferenceScreen prefs = getPreferenceScreen();
 
+        final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());
+        mBlockOnSecureKeyguard = (SwitchPreference) findPreference(PREF_BLOCK_ON_SECURE_KEYGUARD);
+        if (lockPatternUtils.isSecure()) {
+            mBlockOnSecureKeyguard.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD, 1) == 1);
+            mBlockOnSecureKeyguard.setOnPreferenceChangeListener(this);
+        } else {
+            prefs.removePreference(mBlockOnSecureKeyguard);
+        }
+
     }
 
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mBlockOnSecureKeyguard) {
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD,
+                    (Boolean) newValue ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 }
